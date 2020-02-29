c06d02c2f31719f3ba0fad69c61f23c93f8697d1
crystal98tang <tyk502709661@126.com>
2020-03-01 01:51:46 +0800

    fix SDFCN

--- models/SDFCN.py
+++ models/SDFCN.py
@@ -1,9 +1,10 @@
 from models.utils import *
 
-def SDFCN(input_size = (imageSize,imageSize,Channels)):
+
+def SDFCN(input_size=(imageSize,imageSize,Channels)):
     inputs = Input(input_size)
-    conv1_1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1_2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1_1)
+    conv1_1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1_2 = Conv2D(64, 3, activation='relu', padding='same')(conv1_1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)
 
     sc2 = shortcutblock(128)(pool1)
@@ -15,15 +16,15 @@ def SDFCN(input_size = (imageSize,imageSize,Channels)):
     sc4 = shortcutblock(512)(pool3)
     sc4 = shortcutblock(512)(sc4)
 
-    conv5_1 = Conv2DTranspose(256, 3, strides=2, padding='same', kernel_initializer='he_normal')(Concatenate()([sc4, pool3]))
+    conv5_1 = Conv2DTranspose(256, 3, strides=2, padding='same')(Concatenate()([sc4, pool3]))
     conv5_2 = shortcutblock(256)(conv5_1)
 
-    conv6_1 = Conv2DTranspose(128, 3, strides=2, padding='same', kernel_initializer='he_normal')(Concatenate()([conv5_2, pool2]))
+    conv6_1 = Conv2DTranspose(128, 3, strides=2, padding='same')(Concatenate()([conv5_2, pool2]))
     conv6_2 = shortcutblock(128)(conv6_1)
 
-    conv7_1 = Conv2DTranspose(64, 3, strides=2, padding='same', kernel_initializer='he_normal')(Concatenate()([conv6_2, pool1]))
-    conv7_2 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7_1)
-    conv7_3 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7_2)
+    conv7_1 = Conv2DTranspose(64, 3, strides=2, padding='same')(Concatenate()([conv6_2, pool1]))
+    conv7_2 = Conv2D(64, 3, activation='relu', padding='same')(conv7_1)
+    conv7_3 = Conv2D(64, 3, activation='relu', padding='same')(conv7_2)
 
     # softmax
     conv8 = Conv2D(Classes, 1, activation='softmax')(conv7_3)
--- models/utils.py
+++ models/utils.py
@@ -1,12 +1,13 @@
 import args
+import tensorflow as tf
 # If can not work, add "tensorflow.python."
 from tensorflow.python.keras.models import *
 from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.optimizers import *
 #
-#from keras.models import *
-#from keras.layers import *
-#from keras.optimizers import *
+# from keras.models import *
+# from keras.layers import *
+# from keras.optimizers import *
 """
     Image Size
 """
@@ -24,12 +25,12 @@ Classes = args.global_label_classes
 """
 def shortcutblock(filter):
     def _create_shortcut_block(inputs):
-        conv_main = Conv2D(filter, 1, padding='same', kernel_initializer='he_normal')(inputs)
-        conv_main = Conv2D(filter, 3, padding='same', kernel_initializer='he_normal')(conv_main)
-        conv_main = Conv2D(filter, 1, padding='same', kernel_initializer='he_normal')(conv_main)
+        conv_main = Conv2D(filter, 1, padding='same')(inputs)
+        conv_main = Conv2D(filter, 3, padding='same')(conv_main)
+        conv_main = Conv2D(filter, 1, padding='same')(conv_main)
         conv_main = BatchNormalization()(conv_main)
 
-        conv_fine = Conv2D(filter, 1, padding='same', kernel_initializer='he_normal')(inputs)
+        conv_fine = Conv2D(filter, 1, padding='same')(inputs)
         conv_fine = BatchNormalization()(conv_fine)
 
         merge = Add()([conv_main, conv_fine])
